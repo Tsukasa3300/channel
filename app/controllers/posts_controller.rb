a3d0ee76dc0070a0bfcb004dadfa477d1a7f9e9c
Tsukasa3300 <120322648+Tsukasa3300@users.noreply.github.com>
2023-02-15 17:06:59 +0900

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :correct_user,   only: :destroy
+  before_action :logged_in_user, only: [:index, :destroy, :create]
   def index
   @posts=Post.all
 end
@@ -22,9 +24,22 @@ def create
 end
 
 def destroy
+  @post.destroy
+        flash[:success] = "投稿を削除しました"
+        if request.referrer.nil?
+          redirect_to root_url, status: :see_other
+        else
+          redirect_to request.referrer, status: :see_other
+        end
 end
 
 def post_params
   params.require(:post).permit(:content)
+end
+
+
+def correct_user
+  @post = current_user.posts.find_by(id: params[:id])
+  redirect_to root_url, status: :see_other if @post.nil?
 end
 end
