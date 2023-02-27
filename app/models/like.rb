1fa478e9c798998dc35c20bcb28ac735853b92db
Tsukasa3300 <120322648+Tsukasa3300@users.noreply.github.com>
2023-02-27 15:58:19 +0900

    add

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
   belongs_to :comment, optional: true
-
   validates :user_id, presence: true
   validates :post_id, presence: true
 end
